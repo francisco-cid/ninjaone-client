d8a285f7718eb3f66b18fb718e117fe257c333fb
francisco-cid <60446619+francisco-cid@users.noreply.github.com>
2024-11-08 15:04:30 -0700

    initial react app setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  document.title = 'Testing'
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a react app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

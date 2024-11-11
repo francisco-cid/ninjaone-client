95d6d6bf03d26ed8fc3cbec495dc65767c3de324
francisco-cid <60446619+francisco-cid@users.noreply.github.com>
2024-11-11 10:18:10 -0700

    implement hover effect on table rows

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,12 +1,16 @@
-import DeviceDetails from "./DeviceDetails/DeviceDetails";
+import DeviceDetails from './DeviceDetails/DeviceDetails';
+import { useState } from 'react';
 import './DataTable.css'
 
 const DataTable = ({devices}) => {
+    // determines which row if any should display action menu
+    const [hoveredRow, setHoveredRow] = useState(null)
     return (
         <table className="device-table">
             <thead>
             <tr>
                 <th>Device</th>
+                <th/> {/* Empty header for the action column */}
             </tr>
             </thead>
             <tbody>
@@ -14,10 +18,22 @@ const DataTable = ({devices}) => {
                 // don't display row if device details are missing
                 if(!(device.system_name && device.hdd_capacity && device.type)) return false;
                 return (
-                <tr key={device.id}>
-                    <td className="device-details">
+                <tr key={device.id}
+                    className="device-row"
+                    onMouseEnter={() => setHoveredRow(device.id)}
+                    onMouseLeave={() => setHoveredRow(null)}
+                >
+                    <td>
                         <DeviceDetails name={device.system_name} type={device.type} capacity={device.hdd_capacity} />
                     </td>
+                    <td className="action-cell">
+                    {hoveredRow === device.id && (
+                            <button>
+                                ActionButton
+                            </button>
+                    )}
+                     </td>
+                    
                 </tr>
             )})}
             </tbody>

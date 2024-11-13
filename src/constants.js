7c29cdfcd32fa21737e72a378cb3f49dbdcc5e9a
francisco-cid <60446619+francisco-cid@users.noreply.github.com>
2024-11-12 19:47:12 -0700

    add dropdown options to constants

--- src/constants.js
+++ src/constants.js
@@ -13,3 +13,18 @@ export const SORT_OPTIONS = {
   NAME_ASCENDING: "NAME,ASC",
   NAME_DESCENDING: "NAME,DESC",
 };
+
+// passed to select component includes user facing text
+export const FILTER_DROPDOWN_OPTIONS = [
+  { value: DEVICE_TYPES.ANY, text: "All" },
+  { value: DEVICE_TYPES.WINDOWS, text: "Windows" },
+  { value: DEVICE_TYPES.MAC, text: "Mac" },
+  { value: DEVICE_TYPES.LINUX, text: "Linux" },
+];
+
+export const SORT_DROPDOWN_OPTIONS = [
+  { value: SORT_OPTIONS.CAP_DESCENDING, text: "HDD Capacity (Descending)" },
+  { value: SORT_OPTIONS.CAP_ASCENDING, text: "HDD Capacity (Ascending)" },
+  { value: SORT_OPTIONS.NAME_DESCENDING, text: "Name (Descending)" },
+  { value: SORT_OPTIONS.NAME_ASCENDING, text: "Name (Ascending)" },
+];
